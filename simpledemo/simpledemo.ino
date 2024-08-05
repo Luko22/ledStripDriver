35aec6f8868e3a95e1ba522209070ee719b52d8c
laudoluk <Laudo-Finda-Pedro.Lukoki@hsrw.org>
2024-08-05 17:05:34 +0200

    Update simpledemo.ino

--- simpledemo/simpledemo.ino
+++ simpledemo/simpledemo.ino
@@ -1,16 +1,10 @@
-// Define pin numbers for the LEDs
-#include <cvzone.h>
 
-SerialData serialData(3,1);
-
-int valsRec[3]; // array of int with size numOfValsRec 
 
 #define GREEN_PIN 11
 #define RED_PIN 10
 #define BLUE_PIN 9
 
 void setup() {
-  serialData.begin();
 
   // Set pin modes for the LEDs
   pinMode(GREEN_PIN, OUTPUT);
@@ -36,26 +30,25 @@ void setup() {
 
 void loop() {
   
-  serialData.Get(valsRec);
+  analogWrite(BLUE_PIN, 255);
+  analogWrite(GREEN_PIN, 255);
+  analogWrite(RED_PIN, 255);
+  delay(500);
 
-  // Turn on all LEDs to their maximum brightness
-  analogWrite(BLUE_PIN, valsRec[0]);
-  analogWrite(GREEN_PIN, 0);
-  analogWrite(RED_PIN, 0);
+  int b;
+  for (b=0; b<255; b++){
+    analogWrite(BLUE_PIN, 255);
+    analogWrite(GREEN_PIN, 255-b);
+    analogWrite(RED_PIN, 255-b);
+    delay(50);
+  }
 
-  int Test = valsRec[0];
-
-  // Print analog readings for each LED
-  Serial.print("LED:");
-  Serial.print(analogRead(BLUE_PIN));
-  Serial.print(" | ");
-  Serial.println(valsRec[0]);
-
-  // Serial.print("Red: ");
-  // Serial.print(analogRead(RED_PIN));
-  // Serial.print(" | ");
-  // Serial.print("Blue: ");
-  // Serial.println(analogRead(BLUE_PIN));
-
+  int p;
+  for (p=0; p<255; p++){
+    analogWrite(BLUE_PIN, 255);
+    analogWrite(GREEN_PIN, p);
+    analogWrite(RED_PIN, p);
+    delay(50);
+  }
  // Delay for readability
 }
