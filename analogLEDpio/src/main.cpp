eb5d7430f8d82505b46c66630f3c16c66e4e8b97
laudoluk <Laudo-Finda-Pedro.Lukoki@hsrw.org>
2024-09-19 20:40:37 +0200

    Blynk server connectivity assured

--- analogLEDpio/src/main.cpp
+++ analogLEDpio/src/main.cpp
@@ -17,39 +17,65 @@ const char *password = "Duisbu5.";
 #define GREEN 26
 #define BLUE 25
 
+int redPin, greenPin, bluePin;
+BLYNK_WRITE(V0) {
+  redPin = param.asInt();
+}
+
+BLYNK_WRITE(V1) {
+  greenPin = param.asInt();
+}
+
+BLYNK_WRITE(V2) {
+  bluePin = param.asInt();
+}
 void setup() {
   // put your setup code here, to run once:
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);
   pinMode(BLUE, OUTPUT);
 
-  // analogWrite(RED, 0);
-  // analogWrite(GREEN, 0);
-  // analogWrite(BLUE, 0);
-  // delay(3000);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, password);
+  Serial.println();
+  delay(1000);
+
+  analogWrite(RED, 0);
+  analogWrite(GREEN, 0);
+  analogWrite(BLUE, 0);
+  delay(500);
+  analogWrite(RED, 255);
+  analogWrite(GREEN, 255);
+  analogWrite(BLUE, 255);
+  delay(500);
+  analogWrite(RED, 0);
+  analogWrite(GREEN, 0);
+  analogWrite(BLUE, 0);
+  delay(500);
+  analogWrite(RED, 255);
+  analogWrite(GREEN, 255);
+  analogWrite(BLUE, 255);
+  delay(500);
+  analogWrite(RED, 0);
+  analogWrite(GREEN, 0);
+  analogWrite(BLUE, 0);
+  delay(500);
+  analogWrite(RED, 255);
+  analogWrite(GREEN, 255);
+  analogWrite(BLUE, 255);
+  delay(500);
 
 }
 
 void loop() {
+  Blynk.run();
   // put your main code here, to run repeatedly:
-  analogWrite(RED, 255);
-  analogWrite(GREEN, 255);
-  analogWrite(BLUE, 255);
-  delay(1000);
-  analogWrite(RED, 0);
-  analogWrite(GREEN, 0);
-  analogWrite(BLUE, 0);
-  delay(1000);
-  analogWrite(RED, 255);
-  analogWrite(GREEN, 0);
-  analogWrite(BLUE, 0);
-  delay(1000);  
-  analogWrite(RED, 0);
-  analogWrite(GREEN, 255);
-  analogWrite(BLUE, 0);
-  delay(1000);
-  analogWrite(RED, 0);
-  analogWrite(GREEN, 0);
-  analogWrite(BLUE, 255);
-  delay(1000);
+  analogWrite(RED, redPin);
+  analogWrite(GREEN, greenPin);
+  analogWrite(BLUE, bluePin);
+  delay(10);
+
+  Serial.print("RED: "); Serial.print(redPin); Serial.print(" | ");
+  Serial.print("GREEN: "); Serial.print(greenPin); Serial.print(" | ");
+  Serial.print("BLUE: "); Serial.print(bluePin); Serial.print(" | ");
+  Serial.println();
 }
